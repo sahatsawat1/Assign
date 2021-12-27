bdbe4117674b251abe8ccc984bfeecd5e11dc272
sahatsawatF <76928286+sahatsawatF@users.noreply.github.com>
2021-12-27 10:14:07 +0700

    first commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Nav } from "reactstrap";
+import PerfectScrollbar from "perfect-scrollbar";
+
+import logo from "logo.png";
+
+var ps;
+
+function Sidebar(props) {
+  const sidebar = React.useRef();
+  const activeRoute = (routeName) => {
+    return props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  };
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(sidebar.current, {
+        suppressScrollX: true,
+        suppressScrollY: false,
+      });
+    }
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+      }
+    };
+  });
+  return (
+    <div className="sidebar" data-color={props.backgroundColor}>
+      <div className="logo">
+        <a
+          className="simple-text logo-mini"
+          target="_blank"
+        >
+          <div className="logo-img">
+            <img src={logo} alt="react-logo" />
+          </div>
+        </a>
+        <a
+          className="simple-text logo-normal"
+          target="_blank"
+        >
+          Suck Your D
+        </a>
+      </div>
+      <div className="sidebar-wrapper" ref={sidebar}>
+        <Nav>
+          {props.routes.map((prop, key) => {
+            if (prop.redirect) return null;
+            return (
+              <li
+                className={
+                  activeRoute(prop.layout + prop.path) +
+                  (prop.pro ? " active active-pro" : "")
+                }
+                key={key}
+              >
+                <NavLink
+                  to={prop.layout + prop.path}
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  <i className={"now-ui-icons " + prop.icon} />
+                  <p>{prop.name}</p>
+                </NavLink>
+              </li>
+            );
+          })}
+        </Nav>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
